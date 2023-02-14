60038c3241715960a70802c027e97ac82a7e2783
divij <divij.827@gmail.com>
2023-02-14 00:36:39 -0500

    initial commit

---
+++ pages/register/index.js
@@ -0,0 +1,81 @@
+import fAuth from "../../firebase/clientApp";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const createUser = (email, pass) => {
+    createUserWithEmailAndPassword(fAuth, email, pass)
+    .then((userCredential) => {
+      // console.log("Signed in ", userCredential)
+      alert('registered sucessfully')
+    })
+    .catch((error) => {
+      alert('registered error pls try again')
+  });
+}
+
+function handleSubmit (e) {
+  e.preventDefault();
+
+  const fields = {
+    email: e.target.email.value,
+    pass: e.target.password.value,
+    confirmPass: e.target['confirm-password'].value
+  }
+
+  console.log(fields)
+
+  if(validateForm(fields)) {
+    createUser(fields.email, fields.pass)
+  }
+}
+
+function validateForm (fields) {
+  if(fields.confirmPass !== fields.pass) {
+    alert('Password doesnt match')
+    return false;
+  }
+  return true;
+}
+
+export default function Register() {
+  return (
+    <>
+      <section className="bg-gray-50 light:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div className="w-full bg-white rounded-lg shadow light:border md:mt-0 sm:max-w-md xl:p-0 light:bg-gray-800 light:border-gray-700">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl light:text-white">
+                        Create and account
+                    </h1>
+                    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 light:text-white">Your email</label>
+                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" placeholder="name@company.com" required/>
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 light:text-white">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" required/>
+                        </div>
+                        <div>
+                            <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 light:text-white">Confirm password</label>
+                            <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" required/>
+                        </div>
+                        <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                              <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 light:bg-gray-700 light:border-gray-600 light:focus:ring-primary-600 light:ring-offset-gray-800" required=""/>
+                            </div>
+                            <div className="ml-3 text-sm">
+                              <label htmlFor="terms" className="font-light text-gray-500 light:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline light:text-primary-500" href="#">Terms and Conditions</a></label>
+                            </div>
+                        </div>
+                        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center light:bg-primary-600 light:hover:bg-primary-700 light:focus:ring-primary-800">Create an account</button>
+                        <p className="text-sm font-light text-gray-500 light:text-gray-400">
+                            Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline light:text-primary-500">Login here</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
